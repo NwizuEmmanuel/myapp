f1fc41aeab4a140dc43fa12d71e0b39ab93af160
NwizuEmmauel <nwizuemmanuel200@gmail.com>
2022-07-13 12:52:43 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import React from "react";
-import { useState } from "react";
+import Note from "./components/Note";
 
-const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClicks = () => {
-    setAll(allClicks.concat("L"));
-    setLeft(left + 1);
-  };
-  const handleRightClicks = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
-  };
-
-  const History = (props) => {
-    if (props.allClicks.length === 0) {
-      return <div>The app is used by pressing the buttons</div>;
-    }
-    return <div>button press history: {props.allClicks.join(" ")}</div>;
-  };
-
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>{text}</button>
-  );
-
+const App = ({ notes }) => {
   return (
-    <div>
-      {left}
-      <Button handleClick={handleLeftClicks} text="left" />
-      {right}
-      <Button handleClick={handleRightClicks} text="right" />
-      <History allClicks={allClicks} />
-    </div>
+    <>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </>
   );
 };
 
